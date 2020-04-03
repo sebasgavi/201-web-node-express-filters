f876854c986656391bfe9de4869588fffdc4fa3a
Sebastián Gaviria <sebastiangaviria@outlook.com>
2020-04-03 07:52:32 -0500

    list render

---
+++ products.js
@@ -0,0 +1,34 @@
+var products = [
+  {
+    name: 'producto 1',
+    price: 10000,
+    weight: 2000,
+    rating: 3.6,
+  },
+  {
+    name: 'producto 2',
+    price: 100000,
+    weight: 1000,
+    rating: 4.6,
+  },
+  {
+    name: 'producto 3',
+    price: 20000,
+    weight: 3000,
+    rating: 3.9,
+  },
+  {
+    name: 'producto 4',
+    price: 160000,
+    weight: 5000,
+    rating: 3.6,
+  },
+  {
+    name: 'producto 5',
+    price: 80000,
+    weight: 1000,
+    rating: 2.2,
+  },
+];
+
+module.exports = products;
--- server.js
+++ server.js
@@ -2,15 +2,21 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
 
+const products = require('./products');
+
 const app = express();
+
+app.use(express.static('public'));
 
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
 
 app.get('/', function (req, res) {
-  res.render('list');
+  var context = {
+    list: products,
+  }
+  res.render('list', context);
 });
-
 
 app.listen(3000, function(){
   console.log('app escuchando.');
