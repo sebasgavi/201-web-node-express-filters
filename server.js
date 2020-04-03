84ce8d0dce01eae3ba9b6d092c8559433a4f48ad
Sebastián Gaviria <sebastiangaviria@outlook.com>
2020-04-03 08:18:05 -0500

    comments

--- server.js
+++ server.js
@@ -2,12 +2,16 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
 
+// importar productos
 const products = require('./products');
 
+// crear servidor
 const app = express();
 
+// configurar carpeta pública
 app.use(express.static('public'));
 
+// configurar handlebars
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
 
@@ -15,12 +19,17 @@ app.set('view engine', 'handlebars');
 // ?weight_lt=4000 -> se utiliza para variables opcionales (leemos en req.query.weight_lt)
 
 app.get('/', function (req, res) {
+  // imprimir todas las variables del query
   console.log(req.query);
 
+  // arreglo filtrado
   var filtered;
 
+  // si el usuario pidió filtrar por precio
   if(req.query.price_lt){
+    // creo la copia del arreglo filtrado
     filtered = products.filter(function (elem) {
+      // si el precio del elemento es menor al precio que el usuario preguntó
       if(elem.price < req.query.price_lt){
         return true;
       }
@@ -29,18 +38,22 @@ app.get('/', function (req, res) {
 
   if(req.query.search){
     filtered = products.filter(function (elem) {
+      // si el nombre del producto incluye lo que el usuario buscó
       if(elem.name.includes(req.query.search)){
         return true;
       }
     });
   }
 
+  // crear el contexto
   var context = {
     list: filtered,
   }
+  // renderizar el archivo list.handlebars con el contexto creado
   res.render('list', context);
 });
 
+// iniciar servidor en puerto 3000
 app.listen(3000, function(){
   console.log('app escuchando.');
 });
